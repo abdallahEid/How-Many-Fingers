242c127b6ecb235de3aa5464b164d4b9ab0b7909
Abdallah Eid <abdallah@Abdallahs-iMac.local>
2017-09-27 13:07:46 +0200

    Commits the whole project

--- How Many Fingers ?/ViewController.swift
+++ How Many Fingers ?/ViewController.swift
@@ -10,6 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var textField: UITextField!
+    
+    @IBAction func guessButton(_ sender: Any) {
+        
+        let myRand = arc4random_uniform(6)
+        
+        if String(myRand) == textField.text {
+            
+            resultLabel.text = "You're Right!"
+            
+        }else{
+            
+            resultLabel.text = "You're Wrong, It was \(myRand)"
+            
+        }
+        
+    }
+    
+    @IBOutlet var resultLabel: UILabel!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
